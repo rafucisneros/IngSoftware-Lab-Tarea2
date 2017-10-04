3684d8bc1924631c1871b4ce393f57bee12640f2
Rafael Cisneros <rafucho_A_cisneros@hotmail.com>
2017-10-03 20:14:51 -0400

    inicio de la tarea

---
+++ classServicio.py
@@ -0,0 +1,13 @@
+'''
+Created on 3 oct. 2017
+
+@author: Rafael Cisneros
+'''
+
+class Servicio():
+    
+    def __init__(self):
+        pass
+    
+    def calcularPrecio(self, tarifa, tiempoDeServicio):
+        pass
---
+++ classTarifa.py
@@ -0,0 +1,11 @@
+'''
+Created on 3 oct. 2017
+
+@author: Rafael Cisneros
+'''
+
+class Tarifa():
+    
+    def __init__(self, tarifaDiaSemana, tarifaFinDeSemana):
+        self.tarifaDiaSemana = tarifaDiaSemana
+        self.tarifaFinDeSemana = tarifaFinDeSemana
